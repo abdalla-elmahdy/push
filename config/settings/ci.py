926038d1b2f0c86ef664f148187733df5714d181
Abdalla Elmahdy <52469229+abdalla-elmahdy@users.noreply.github.com>
2023-07-23 14:42:18 +0300

    Added simple CI workflow to run tests - added the workflow config file - added a separate requirements and settings files for ci environment

---
+++ config/settings/ci.py
@@ -0,0 +1,29 @@
+from .base import *
+
+# These settings should only be used for CI only
+# NOT SUITABLE FOR PRODUCTION
+
+SECRET_KEY = "changemeinproduction"
+
+DEBUG = True
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# Databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+# Static files handling
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
