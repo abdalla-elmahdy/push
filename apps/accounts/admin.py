1e561edb5d2e8f364eec1eb96b40963befab69b4
Abdalla Elmahdy <52469229+abdalla-elmahdy@users.noreply.github.com>
2023-07-24 19:36:13 +0300

    Added custom forms and custom admin for the new user model

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 CustomUser = get_user_model()
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "username",
+        "email",
+        "status",
+        "is_superuser",
+    ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
