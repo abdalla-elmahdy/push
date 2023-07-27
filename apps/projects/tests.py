95b89b3191667ff8441e9899c5608db525da500f
Abdalla Elmahdy <52469229+abdalla-elmahdy@users.noreply.github.com>
2023-07-27 17:55:04 +0300

    Added tests for the project update view

--- apps/projects/tests.py
+++ apps/projects/tests.py
@@ -18,12 +18,23 @@ class ProjectsTests(TestCase):
             bio="My bio",
             password="testpass123",
         )
+        # for tests that need more than one user
+        cls.alice = CustomUser.objects.create_user(
+            username="alice",
+            email="alice@example.com",
+            first_name="Alice",
+            last_name="Malicious",
+            status="PR",
+            bio="My bio",
+            password="testpass123",
+        )
         cls.project = Project.objects.create(
             title="Project Title",
             owner=cls.user,
             description="Project description",
         )
 
+    # detail view tests
     def test_project_detail_view(self):
         response = self.client.get(self.project.get_absolute_url())
         no_response = self.client.get("/projects/1234")
@@ -32,6 +43,7 @@ class ProjectsTests(TestCase):
         self.assertTemplateUsed(response, "projects/detail.html")
         self.assertContains(response, self.project.title)
 
+    # create view tests
     def test_project_create_view_post(self):
         self.client.force_login(self.user)
         data = {
@@ -61,3 +73,43 @@ class ProjectsTests(TestCase):
         response = self.client.get(reverse("projects:create"))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, "projects/create.html")
+
+    # update view tests
+    def test_project_update_view_post(self):
+        self.client.force_login(self.user)
+        data = {
+            "title": "Project Title(UPDATED)",
+            "description": "Project decription",
+            "open_for": "CT",
+            "time_estimate": "DY",
+        }
+        response = self.client.post(
+            reverse("projects:update", args=[self.project.id]),
+            data,
+        )
+        self.assertEquals(response.status_code, 302)
+        self.project.refresh_from_db()
+        self.assertEquals(self.project.title, "Project Title(UPDATED)")
+
+    def test_project_update_view_get(self):
+        self.client.force_login(self.user)
+        response = self.client.get(
+            reverse("projects:update", args=[str(self.project.id)])
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, "projects/update.html")
+
+    def test_only_owner_can_update_project(self):
+        self.client.force_login(self.alice)
+        data = {
+            "title": "Alice's Project",
+            "description": "Project decription",
+            "open_for": "CT",
+            "time_estimate": "DY",
+        }
+        response = self.client.post(
+            reverse("projects:update", args=[self.project.id]),
+            data,
+        )
+        self.assertEquals(response.status_code, 403)
+        self.assertNotEquals(self.project.title, "Alice's Project")
