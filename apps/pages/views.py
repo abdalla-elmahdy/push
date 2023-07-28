03127e5a2b23682a611b813692d4e072733ccfde
Abdalla Elmahdy <52469229+abdalla-elmahdy@users.noreply.github.com>
2023-07-28 10:00:05 +0300

    Added the search form - created a new form class for the search form - changed HomePageView to a FormView instead of TemplateView - rendered the form on home page template, doesn't do anything for now

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+
+from .forms import SearchForm
 
 
-class HomePageView(TemplateView):
+class HomePageView(FormView):
+    """
+    Displays an intro section about the platform and search form for projects
+    Context:
+        - form: an instance of SearchForm
+    """
+
     template_name = "pages/home.html"
+    form_class = SearchForm
