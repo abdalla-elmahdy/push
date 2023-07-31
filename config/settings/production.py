0b212607101d697ecd13dcdf9397857afce09f2f
Abdalla Elmahdy <52469229+abdalla-elmahdy@users.noreply.github.com>
2023-08-01 00:17:49 +0300

    Added production configs and requirements file for deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,26 @@ from decouple import config
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 DEBUG = False
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DBNAME"),
+        "USER": config("DBUSER"),
+        "PASSWORD": config("DBPASS"),
+        "HOST": config("DBHOST"),
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
